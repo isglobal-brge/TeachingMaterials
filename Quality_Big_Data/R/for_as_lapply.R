fc2b7d74f197086d40b7f60cb80cd32c3da71189
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2019-02-23 14:30:00 +0100

    uploaded for and lapply

---
+++ Quality_Big_Data/R/for_as_lapply.R
@@ -0,0 +1,31 @@
+x <- matrix(rnorm(100000000), nrow=100)
+dim(x)
+
+
+meanF <- function(x){
+ ans <- rep(NA, ncol(x))
+ for (i in 1:ncol(x))
+  ans[i] <- mean(x[,i])
+ ans
+}
+
+meanF2 <- function(i, x){
+  mean(x[,i])
+}
+
+system.time(meanF(x))
+
+system.time(lapply(1:ncol(x), meanF2, x=x))
+
+
+#
+# Ejemplo glm y lm
+#
+
+library(Rfast)
+library(microbenchmark)
+bench <- microbenchmark(glm = glm(uptake ~ conc, data=CO2),
+                        lm = lm(uptake ~ conc, data=CO2),
+                        Rfast = lmfit(CO2$uptake, CO2$conc))
+bench
+plot(bench)
