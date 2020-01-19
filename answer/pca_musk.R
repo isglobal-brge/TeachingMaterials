2bae1a89dfe4f310bf9153127aed56636fd07ee7
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2020-01-19 18:13:24 +0100

    answer pca_musk

--- answer/pca_musk.R
+++ answer/pca_musk.R
@@ -1,7 +1,24 @@
-dd <- read.delim("c:/Juan/CREAL/GitHub/TeachingMaterials/Master_Modelling/data/musk.txt")
+
+
+
+# load library
 library(factoextra)
+
+
+# read data (change path)
+pp <- "c:/Juan/CREAL/GitHub/TeachingMaterials/Master_Modelling/data/"
+dd <- read.delim(file.path(pp, "musk.txt"))
+head(dd)
+ncol(dd)
+
+# create factor variable (with labels)
+dd$group <- factor(dd$musk, label=c("non-musk", "musk"))
+
 o <- which(colnames(dd)=="musk")
-group <- dd[o]
-mycol <- ifelse(group==1, "darkgreen", "blue")
-pp <- prcomp(dd[ , -o])
-fviz_pca_ind(pp, col.ind = mycol)
+pca <- prcomp(dd[ , -o])
+
+# individuals
+fviz_pca_ind(pca, col.ind = dd$group, legend.title="Musk")
+
+# variables
+fviz_pca_var(pca)
